63ed374898ecf034ebe0bdb034f046c0ef0b0876
fstg0504 <598846720@qq.com>
2019-06-27 23:17:45 +0800

    测试skline

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ const fs=require("fs");
 const app = express();
 app.use(express.static("public"));
 
+//设置跨域访问
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    if (req.method === 'OPTIONS') {
+        res.send(200)
+    } else {
+        next()
+    }
+});
 // 使用fs.readFile打开html文件
 app.get("/", function(request, response) {
     response.writeHead(200,{"Content-Type":"text/html"});
